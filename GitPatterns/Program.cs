2eb674c503d60fd8638a178cbc7906f3d9fdc504
Zdzichu Testowy <zdzichukkwal@gmail.com>
2017-04-06 18:32:54 +0200

    Changed project structure

--- GitPatterns/Program.cs
+++ GitPatterns/Program.cs
@@ -7,97 +7,6 @@ using System.Threading.Tasks;
 
 namespace GitPatterns
 {
-    enum LoggerType
-    {
-        Cool,
-        Bad
-    }
-
-    interface ILog
-    {
-        void Log();
-    }
-
-    interface ILogBuilder
-    {
-        void BuildLevel();
-        Logger Logger { get; }
-    }
-
-    class Builder
-    {
-        public Logger Build(ILogBuilder LogBuilder)
-        {
-            LogBuilder.BuildLevel();
-            return LogBuilder.Logger;
-        }
-    }
-
-    abstract class Logger : ILog
-    {
-        public virtual int level { get; set; } // some abstract prop
-        public virtual void Log()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    sealed class CoolLoggerBuilder : ILogBuilder
-    {
-        private Logger _logger;
-
-        public CoolLoggerBuilder()
-        {
-            _logger = new CoolLogger();
-        }
-
-        public Logger Logger
-        {
-            get
-            {
-                return _logger;
-            }
-        }
-
-        public void BuildLevel()
-        {
-            _logger.level = 5;
-        }
-    }
-
-    class CoolLogger : Logger
-    {
-        public override void Log()
-        {
-            Console.WriteLine("Hi hello!");
-        }
-    }
-
-    class BadLogger : Logger
-    {
-        public override void Log()
-        {
-            Console.WriteLine("Bad bad..");
-        }
-    }
-
-    /*
-     * factory method 
-     */
-     
-    class LoggerFactory
-    {
-        public Logger CreateLogger (LoggerType type)
-        {
-            switch (type)
-            {
-                case LoggerType.Cool:
-                    return new CoolLogger();
-                default:
-                    return new BadLogger();
-            }
-        }
-    }
 
     class Program
     {
@@ -120,9 +29,11 @@ namespace GitPatterns
             //Console.WriteLine(logger.level);
             //logger.Log();
 
-            LoggerFactory loggerFactory = new LoggerFactory();
-            logger = loggerFactory.CreateLogger(LoggerType.Cool);
-            logger.Log();
+            // for factory method example
+
+            //LoggerFactory loggerFactory = new LoggerFactory();
+            //logger = loggerFactory.CreateLogger(LoggerType.Cool);
+            //logger.Log();
         }
     }
 }
